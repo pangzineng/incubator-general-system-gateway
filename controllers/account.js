dea0b1cf3711169127013c818cf24646020ec48f
Peter Pang <pang0144@e.ntu.edu.sg>
2019-03-12 18:28:00 +0800

    Bugfix: replace bcrypt with bcryptjs, because is was hard to install
    there were a lot of problems installing bcrypt, it won't work in many cases, affected by the version of node, os and even hardware

--- controllers/account.js
+++ controllers/account.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 var Account = require('../models/account');
 var Profile = require('../models/profile');
 var Permission = require('../models/permission');
